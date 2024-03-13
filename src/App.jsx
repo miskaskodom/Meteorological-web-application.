57b11cad81a414e6a95c9c7c10b43b688786192d
Dmitri Shelukhin <dima.okey@gmail.com>
2024-03-14 01:00:18 +0300

    try to fix router error with HashRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import {  HashRouter, Route, Routes } from "react-router-dom";
 import StartPage from "./pages/StartPage";
 import WeatherAppPage from "./pages/WeatherAppPage";
 import AboutApp from "./pages/AboutApp";
@@ -7,14 +7,14 @@ import Setting from "./pages/Settings";
 
 function App() {
   return (
-    <BrowserRouter basename="/Meteorological-web-application./">
+    <HashRouter basename="/Meteorological-web-application./">
       <Routes>
         <Route path="*" element={<StartPage />} />
         <Route basename='/Meteorological-web-application./setting' path="/setting" element={<Setting />} />
         <Route path="/mainapp" element={<WeatherAppPage />} />
         <Route path="/aboutapp" element={<AboutApp />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
